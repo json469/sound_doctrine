402bd49a9a25034642ec80ad2f451526803e2257
Jesse <json469@aucklanduni.ac.nz>
2019-02-28 22:04:43 +1300

    Edit file architecture

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import './data/heidelberg.dart';
-import './data/lordsday.dart';
 import 'navigation_drawer.dart';
-import 'lordsday_page.dart';
+import 'heidelberg/heidelberg_page.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -11,42 +9,8 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Project Heidelberg'),
       ),
-      body: _buildListView(),
+      body: HeidelbergPage(),
       drawer: NavigationDrawer(),
     );
-  }
-
-  Widget _buildListView() {
-    return ListView.builder(
-      itemCount: Heidelberg.items.length ?? 0,
-      itemBuilder: (BuildContext context, int index) {
-        return _buildExpandableListView(context, index);
-      },
-    );
-  }
-
-  Widget _buildExpandableListView(BuildContext context, int index) {
-    return ExpansionTile(
-      title: Text(Heidelberg.items[index].header),
-      children:
-          _buildExpandedListView(context, Heidelberg.items[index].lordsDays),
-    );
-  }
-
-  List<Widget> _buildExpandedListView(
-      BuildContext context, List<LordsDay> lordsDays) {
-    List<Widget> expandedList = [];
-    lordsDays.forEach((lordsDay) {
-      expandedList.add(
-        ListTile(
-          title: Text('${lordsDay.header}'),
-          onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => LordsDayPage(lordsDay: lordsDay))),
-        ),
-      );
-    });
-    return expandedList;
   }
 }
