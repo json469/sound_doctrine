d9ad72798e3e9adf5bd5d987de7b20c509007685
Jesse <json469@aucklanduni.ac.nz>
2019-02-28 22:31:16 +1300

    Edit padding

--- lib/apostlescreed/apostlescreed_page.dart
+++ lib/apostlescreed/apostlescreed_page.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import './data/apostlescreed.dart';
 
 class ApostlesCreedPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("The Apostles' Creed"),
       ),
-      body: Text(ApostlesCreed.item)
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Text(ApostlesCreed.item),
+      ),
     );
   }
 }
