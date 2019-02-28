0a9dad2d9331047e65886e6182b80b8f1f3ce99b
Jesse <json469@aucklanduni.ac.nz>
2019-03-01 10:21:32 +1300

    Edit title style on About

--- lib/about.dart
+++ lib/about.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('About'),
+        title: Text('About', style: Theme.of(context).textTheme.title),
       ),
       body: Text('About contents...'),
     );
