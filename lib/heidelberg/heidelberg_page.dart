c45a80daa5467b0cacda2ec6cb824b7e21519435
Jesse <json469@aucklanduni.ac.nz>
2019-02-28 23:57:03 +1300

    Edit Heidelberg Page with Sticky Header

--- lib/heidelberg/heidelberg_page.dart
+++ lib/heidelberg/heidelberg_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sticky_headers/sticky_headers.dart';
+
 import './data/heidelberg.dart';
+import './data/section.dart';
 import './data/lordsday.dart';
 import './lordsday.dart';
 
@@ -10,38 +13,40 @@ class HeidelbergPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Heidelberg Catechism (A.D. 1576)'),
       ),
-      body: ListView.separated(
+      body: ListView.builder(
         itemCount: Heidelberg.items.length ?? 0,
         itemBuilder: (BuildContext context, int index) {
-          return _buildSections(context, index);
+          return StickyHeader(
+              header: Container(
+                height: 50.0,
+                color: Colors.blueGrey[700],
+                padding: new EdgeInsets.symmetric(horizontal: 16.0),
+                alignment: Alignment.centerLeft,
+                child: new Text(
+                  '${Heidelberg.items[index].header}',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              content: Column(
+                children: _buildChildItems(context, Heidelberg.items[index]),
+              )
+              );
         },
-        separatorBuilder: (BuildContext context, int index) => Divider(),
       ),
     );
   }
 
-  Widget _buildSections(BuildContext context, int index) {
-    return ExpansionTile(
-      title: Text(Heidelberg.items[index].header),
-      children:
-          _buildExpandedListView(context, Heidelberg.items[index].lordsDays),
-    );
-  }
-
-  List<Widget> _buildExpandedListView(
-      BuildContext context, List<LordsDay> lordsDays) {
-    List<Widget> expandedList = [];
-    lordsDays.forEach((lordsDay) {
-      expandedList.add(
-        ListTile(
-          title: Text('${lordsDay.header}'),
-          onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => LordsDayPage(lordsDay: lordsDay))),
-        ),
-      );
+  List<Widget> _buildChildItems(BuildContext context, Section section) {
+    List<Widget> childItems = [];
+    section.lordsDays.forEach((lordsDay) {
+      childItems.add(ListTile(
+        title: Text('${lordsDay.header}'),
+        onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => LordsDayPage(lordsDay: lordsDay))),
+      ));
     });
-    return expandedList;
+    return childItems;
   }
 }
