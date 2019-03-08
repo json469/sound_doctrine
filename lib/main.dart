2b48e8adfb00f492c9e8460d181bad709c1e744c
Jesse <json469@aucklanduni.ac.nz>
2019-03-09 00:27:50 +1300

    Add Android app icon and remove debugger ribbon

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Project Heidelberg',
       theme: ThemeData(
           fontFamily: 'Open Sans',
